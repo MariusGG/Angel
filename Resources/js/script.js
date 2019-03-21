fda499548cc0bf1ff1214cd90aaa0fa9ca7795bb
Marius Guei <marius.g.guei@gmail.com>
2019-03-21 14:19:13 +0000

    updated locations

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -79,4 +79,15 @@ $(document).ready(function() {
 
   startSlide();
 
+  /* ----------------------------------------------- */
+  /*                  locations                      */
+  /* ----------------------------------------------- */
+
+  var $contents = $('.tab-content');
+$contents.slice(1).hide();
+$('.tab').click(function() {
+  var $target = $('#' + this.id + 'show').show();
+  $contents.not($target).hide();
+});
+
 });
